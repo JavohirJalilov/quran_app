088001779db6926b1dec2f9ac5ef8c3252bfe1f5
Jalilov <javohirjalilov777@gmail.com>
2022-03-01 11:51:43 +0500

    added get_surah_name function and change sort surah

--- read_data.py
+++ read_data.py
@@ -32,24 +32,38 @@ def arabic2uzbek(data_arabic:list, data_uzbek:list):
         data_arabic[i]['text'] = f"{arabic_word}\n\n{uzbek_work}"
     return data_arabic
 
-def sort_surah(data_arabic_and_uzbek:list)->list:
-    surah_list = []
+def get_surah_name(file_name:str):
+    """
+    Read Quran Academy data
+    """
+    # read json data type
+    f = open(file_name, 'r').read().split('\n')
+    surah_name_list = [0]
+    for surah_name in f:
+        surah_name_list.append(surah_name.split(',')[1])
+    return surah_name_list
+
+def sort_surah(data_arabic_and_uzbek:list,surah_name_list:list)->list:
+    
+    # print(surah_name_list)
+    surah_list = dict()
     ayah_list = []
     for ayah in data_arabic_and_uzbek:
-        if ayah['verse'] == 1:
-            surah_list.append(ayah_list)
+        ayah_list.append(ayah)
+        if ayah['verse'] == 1 and ayah['chapter'] != 1:
+            surah_list[surah_name_list[ayah['chapter']-1]] = ayah_list
             ayah_list = []
             ayah_list.append(ayah)
-            
-        else:
-            ayah_list.append(ayah)
-    
-    return surah_list[1:]
+
+    surah_list[surah_name_list[ayah['chapter']]] = ayah_list
+    return surah_list
     
 
 data_arabic = read_quranacademy('data/ara-quranacademy.json')
 data_uzbek = read_quranacademy('data/uzb-muhammadsodikmu.json')
 
 conv = arabic2uzbek(data_arabic, data_uzbek)
-surah_list = sort_surah(conv)
-print(surah_list[2][4])
+surah_name = get_surah_name('surah.txt')
+
+surah_list = sort_surah(conv,surah_name)
+print(len(surah_list))
